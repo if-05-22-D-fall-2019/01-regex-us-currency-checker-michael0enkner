dc8d11d09f710d12fffea5b10056583e688d178c
Peter Bauer <p.bauer@htl-leonding.ac.at>
2020-05-25 03:03:22 +0200

    Checked all tests again

--- src/main/java/UsCurrencyChecker.java
+++ src/main/java/UsCurrencyChecker.java
@@ -1,8 +1,9 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class UsCurrencyChecker {
     public static Matcher getMatcher(String usCurrencyString) {
-        return Pattern.compile("dljf").matcher(usCurrencyString);
+        throw new NotImplementedException();
     }
 }

8fdd6ad010143489207d244b206c4997598293d4
Peter Bauer <p.bauer@htl-leonding.ac.at>
2020-05-25 02:24:23 +0200

    Removed superfluous subfolder

--- UsCurrencyChecker/src/main/java/UsCurrencyChecker.java
+++
@@ -1,7 +0,0 @@
-import java.util.regex.Matcher;
-
-public class UsCurrencyChecker {
-    public static Matcher getMatcher(String usCurrencyString) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-}

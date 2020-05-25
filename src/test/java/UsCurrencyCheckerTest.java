fba02a32f66a161fa2505bfac73cd75b732fb6c6
Peter Bauer <p.bauer@htl-leonding.ac.at>
2020-05-25 02:25:46 +0200

    Added .idea files

---
+++ src/test/java/UsCurrencyCheckerTest.java
@@ -0,0 +1,70 @@
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class UsCurrencyCheckerTest {
+    @Test
+    public void testValidCurrencyString() {
+        checkRegex("$ 149.50");
+    }
+
+    private void checkRegex(final String stringToBeChecked) {
+        checkRegex(stringToBeChecked, stringToBeChecked);
+    }
+
+    private void checkRegex(final String stringToBeChecked, final String expectedMatch) {
+        Matcher matcher = UsCurrencyChecker.getMatcher(stringToBeChecked);
+        assertThat(matcher.find(), is(true));
+        assertThat(matcher.group(), is(expectedMatch));
+        assertThat(matcher.find(), is(false));
+    }
+
+    @Test
+    public void testNoCurrency() {
+        Matcher matcher = UsCurrencyChecker.getMatcher("This is text without any currency information");
+        assertThat(matcher.find(), is(false));
+    }
+
+    @Test
+    public void testNoBlankNoZero() {
+        checkRegex("$149.5");
+    }
+
+    @Test
+    public void testOnlyBlankBetween$AndNumber() {
+        Matcher matcher = UsCurrencyChecker.getMatcher("$x14.5");
+        assertThat(matcher.find(), is(false));
+    }
+
+    @Test
+    public void testNoDecimals() {
+        checkRegex("$ 200");
+    }
+
+    @Test
+    public void testCurrencyEmbeddedInAText() {
+        checkRegex("This makes $ 149.30 in total", "$ 149.30");
+    }
+
+    @Test
+    public void testMoreThanTwoDecimals() {
+        checkRegex("$1.214", "$1.21");
+    }
+
+    @Test
+    public void testShortestCurrencyPossible() {
+        checkRegex("$3");
+    }
+
+    @Test
+    public void testDecimalPointButNoDecimals() {
+        checkRegex("$ 3468.", "$ 3468");
+    }
+
+    @Test
+    public void testCommaInsteadOfPoint() {
+        checkRegex("$ 3,14", "$ 3");
+    }}
